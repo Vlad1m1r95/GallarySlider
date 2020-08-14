8ba8e15e890c2b283e6103bb287d4ce554a6d441
Vlad1m1r95 <52048473+Vlad1m1r95@users.noreply.github.com>
2020-08-15 02:04:14 +0300

    add mops

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React, { useRef, useState, useEffect } from "react";
 import "./styles.css";
 
 export default function App() {
-  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const arr = [
+    "https://kisapes.ru/images/breeds/dogs/mops.jpg",
+    "https://www.tapeciarnia.pl/tapety/normalne/tapeta-mops-w-trawie.jpg",
+    "https://superminiki.com/files/mops_0.jpg",
+    "https://disgustingmen.com/wp-content/uploads/2020/03/the-order-of-the-pug-6.jpg",
+    "https://elle.ua/i/publications/1022/560_292/do-togo-kak-tebya-nakonec-pustyat-k-tarelke-nuzhno-so-vsemi-obshchatsya-kak-mops-agamemnon-na-zvaniy-uzhin-hodil-1838-21405.jpg"
+  ];
   const slidesContainer = useRef();
   const [slides, setSlides] = useState(null);
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -31,17 +37,19 @@ export default function App() {
   };
 
   return (
-    <>
+    <div className="Gallary">
       <div className="slider">
         <div ref={slidesContainer} className="slides">
-          {arr.map((_, index) => (
-            <div id={`slide-${index}`}>{index + 1}</div>
+          {arr.map((image, index) => (
+            <div className="slide">
+              <img className="image" src={image} alt={"мопс"} />
+            </div>
           ))}
         </div>
       </div>
 
       <div className="pagination">
-        {arr.map((_, index) => (
+        {arr.map((image, index) => (
           <>
             {index === 0 && (
               <button
@@ -52,7 +60,9 @@ export default function App() {
             <button
               className="paginationControll"
               onClick={scrollTo.bind(this, index)}
-            ></button>
+            >
+              <img className="image" src={image} alt={"мопс"} />
+            </button>
 
             {index === arr.length - 1 && (
               <button
@@ -63,6 +73,6 @@ export default function App() {
           </>
         ))}
       </div>
-    </>
+    </div>
   );
 }
