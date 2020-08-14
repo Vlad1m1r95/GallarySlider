18db9362ca284f79a9c25da9d8dde30eaf4d0476
Vlad1m1r95 <52048473+Vlad1m1r95@users.noreply.github.com>
2020-08-15 00:52:27 +0300

    fix back next pagination button

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const slidesContainer = useRef();
   const [slides, setSlides] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     setSlides(() => [...slidesContainer.current.children]);
@@ -12,20 +13,23 @@ export default function App() {
 
   const scrollTo = (index) => {
     slides[index].scrollIntoView({ behavior: "smooth" });
+    setCurrentIndex(index);
   };
 
-  const slideBack = (index) => {
-    alert(index);
-    if (index > 0) {
-      slides[index - 1].scrollIntoView({ behavior: "smooth" });
+  const slideBack = () => {
+    if (currentIndex > 0) {
+      slides[currentIndex - 1].scrollIntoView({ behavior: "smooth" });
+      setCurrentIndex(() => currentIndex - 1);
     }
   };
 
-  const slideNext = (index) => {
-    if (index < slides.length - 1) {
-      slides[index + 1].scrollIntoView({ behavior: "smooth" });
+  const slideNext = () => {
+    if (currentIndex < slides.length - 1) {
+      slides[currentIndex + 1].scrollIntoView({ behavior: "smooth" });
+      setCurrentIndex(() => currentIndex + 1);
     }
   };
+
   return (
     <>
       <div className="slider">
@@ -41,7 +45,7 @@ export default function App() {
           <>
             {index === 0 && (
               <button
-                onClick={slideBack.bind(this, index)}
+                onClick={slideBack}
                 className="paginationButton "
               ></button>
             )}
@@ -52,7 +56,7 @@ export default function App() {
 
             {index === arr.length - 1 && (
               <button
-                onClick={slideNext.bind(this, index)}
+                onClick={slideNext}
                 className="paginationButton "
               ></button>
             )}
