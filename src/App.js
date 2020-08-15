4352524d7a05beecb335cc5fe39d179eefcd5963
Vlad1m1r95 <52048473+Vlad1m1r95@users.noreply.github.com>
2020-08-15 03:13:40 +0300

    change

--- src/App.js
+++ src/App.js
@@ -10,12 +10,15 @@ export default function App() {
     "https://elle.ua/i/publications/1022/560_292/do-togo-kak-tebya-nakonec-pustyat-k-tarelke-nuzhno-so-vsemi-obshchatsya-kak-mops-agamemnon-na-zvaniy-uzhin-hodil-1838-21405.jpg"
   ];
   const slidesContainer = useRef();
+  const slidesPaginationContainer = useRef();
   const [slides, setSlides] = useState(null);
+  const [slidesPagination, setSlidesPagination] = useState(null);
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     setSlides(() => [...slidesContainer.current.children]);
-  }, [slidesContainer]);
+    setSlidesPagination(() => [...slidesPaginationContainer.current.children]);
+  }, [slidesContainer, slidesPaginationContainer]);
 
   const scrollTo = (index) => {
     slides[index].scrollIntoView({ behavior: "smooth" });
@@ -25,6 +28,7 @@ export default function App() {
   const slideBack = () => {
     if (currentIndex > 0) {
       slides[currentIndex - 1].scrollIntoView({ behavior: "smooth" });
+      slidesPagination[currentIndex - 1].scrollIntoView({ behavior: "smooth" });
       setCurrentIndex(() => currentIndex - 1);
     }
   };
@@ -32,6 +36,7 @@ export default function App() {
   const slideNext = () => {
     if (currentIndex < slides.length - 1) {
       slides[currentIndex + 1].scrollIntoView({ behavior: "smooth" });
+      slidesPagination[currentIndex + 1].scrollIntoView({ behavior: "smooth" });
       setCurrentIndex(() => currentIndex + 1);
     }
   };
@@ -48,30 +53,21 @@ export default function App() {
         </div>
       </div>
 
-      <div className="pagination slides">
-        {arr.map((image, index) => (
-          <>
-            {index === 0 && (
+      <div className="pagination">
+        <button onClick={slideBack} className="paginationButton "></button>
+        <div ref={slidesPaginationContainer} className="slides">
+          {arr.map((image, index) => (
+            <>
               <button
-                onClick={slideBack}
-                className="paginationButton "
-              ></button>
-            )}
-            <button
-              className="paginationControll"
-              onClick={scrollTo.bind(this, index)}
-            >
-              <img className="image" src={image} alt={"мопс"} />
-            </button>
-
-            {index === arr.length - 1 && (
-              <button
-                onClick={slideNext}
-                className="paginationButton "
-              ></button>
-            )}
-          </>
-        ))}
+                className="paginationControll"
+                onClick={scrollTo.bind(this, index)}
+              >
+                <img className="image" src={image} alt={"мопс"} />
+              </button>
+            </>
+          ))}
+        </div>
+        <button onClick={slideNext} className="paginationButton "></button>
       </div>
     </div>
   );
